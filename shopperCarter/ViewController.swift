b3b689d6a9e0a8cc7aa4e7cf9c6fee9d15154c37
CarlS <carl.show@yahoo.com>
2021-10-29 07:46:02 -0500

    The first real commit--- it's usable, yet unfinished.

--- shopperCarter/ViewController.swift
+++ shopperCarter/ViewController.swift
@@ -7,13 +7,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var shoperCarter : [String] = ["Orange", "Oragne", "Owrang", "Banana"]
+    var shoppersCart : [String] = [""]
+    var r = 0
+    @IBOutlet weak var theNightmare: UITableView!
+    @IBOutlet weak var iHateTableViews: UITableView!
+    @IBOutlet weak var adder: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        theNightmare.delegate = self
+        theNightmare.dataSource = self
+        iHateTableViews.delegate = self
+        iHateTableViews.dataSource = self
+        if shoppersCart.isEmpty
+        {
+            iHateTableViews.isHidden = true
+        }
+        else
+        {
+            iHateTableViews.isHidden = false
+        }
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if tableView == theNightmare
+        {
+            return shoperCarter.count
+        }
+        else
+        {
+            return shoppersCart.count
+        }
+    }
+        func tableView(_ tableView: UITableView, cellForRowAt t: IndexPath) -> UITableViewCell
+    {
+        let c = tableView.dequeueReusableCell(withIdentifier: "celSel", for: t)
+        if tableView == theNightmare
+        {
+            c.textLabel?.text = "\(shoperCarter[t.row])"
+        }
+        else
+        {
+            c.textLabel?.text = "\(shoppersCart[t.row])"
+        }
+        return c
+    }
+    func tableView(_ g: UITableView, didSelectRowAt p: IndexPath)
+    {
+        r = p.row
+        print(r)
+        shoppersCart.append(shoperCarter[r])
+        iHateTableViews.reloadData()
+        iHateTableViews.isHidden = false
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete
+        {
+            if tableView == theNightmare
+            {
+                shoperCarter.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            else
+            {
+                shoppersCart.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
+    @IBAction func addction(_ sender: Any)
+    {
+        shoperCarter.append(adder.text!)
+        theNightmare.reloadData()
+    }
 }
 
